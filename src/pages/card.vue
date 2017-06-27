b4000f063ab82e07d792e2f49851402ae35d1ea5
周宝 <baobaozhou199606@gmail.com>
2017-06-27 17:14:27 +0000

    增加pages

---
+++ src/pages/card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'card',
+    data () {
+      return {
+        msg: 'this is the card component!'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
