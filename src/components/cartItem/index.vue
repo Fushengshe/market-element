ae642661375b600850325e35fa292a2498d22261
周宝 <baobaozhou199606@gmail.com>
2017-07-07 17:04:44 +0800

    cart的布局进行中

---
+++ src/components/cartItem/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cart-item">
+    <div class="cart-item-img">
+      <img class="item-image" src="./gakki.jpg" alt="">
+    </div>
+    <div class="cart-item-info">
+      <h3 class="info-title">商品名称：{{item.title}}</h3>
+      <div class="info-type">具体型号：{{item.type}}</div>
+      <div class="info-store">店铺名称：{{item.store}}</div>
+      <div class="info-money">
+        <h5 class="info-price">单价：{{item.price}}</h5>
+        <div class="info-count">{{item.count}}数量</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'cart-item',
+    data () {
+      return {
+        msg: 'this is the cart-item component!',
+        item: {
+          title: 'this is the test title',
+          type: 'this is the test type info',
+          store: 'this is the test store name',
+          price: 12,
+          count: 12
+        }
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .cart-item {
+    display flex
+    .cart-item-img {
+      flex 1
+      .item-image {
+        width 100%
+      }
+    }
+    .cart-item-info{
+      flex 1
+    }
+  }
+</style>
--- src/pages/home/cart.vue
+++ src/pages/home/cart.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="template">
-    <p>{{msg}}</p>
+  <div class="cart">
+    <h3 class="top-bar">购物车</h3>
+    <cart-item class="cart-item"></cart-item>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import CartItem from '../../components/cartItem/index.vue'
   export default {
     name: 'cart',
     data () {
       return {
         msg: 'this is the cart component!'
       }
+    },
+    components: {
+      CartItem
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .cart {
+    .top-bar {
+      margin 0
+      text-align left
+      border-bottom 3px solid #555
+    }
+    .cart-item{
+      width 90%
+      margin 0 auto
+    }
+  }
+</style>
